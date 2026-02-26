d14e32b5f617aba019d45c5ce7318e7b4f45604e
Tebias-cloud <esteban.vidal.valencia@gmail.com>
2026-02-26 00:52:35 -0300

    a

--- lib/supabase-admin.ts
+++ lib/supabase-admin.ts
@@ -3,5 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
-// Este cliente ignora toda seguridad y NUNCA debe ir al lado del cliente (Navegador)
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
